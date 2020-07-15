e19758db278af1e023ae29b7d096d518c50aa6e2
WyattScottModding <wyatt@wyattscott.com>
2020-07-15 17:28:46 -0400

    Initial commit

---
+++ app/src/main/java/main/UPS_On_Demand/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package main.UPS_On_Demand.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import main.UPS_On_Demand.data.LoginDataSource;
+import main.UPS_On_Demand.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
